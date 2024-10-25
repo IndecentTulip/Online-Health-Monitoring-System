c06edd1967271aab5e77efd036a4970f90c1c12d
BAntalik <stich102@hotmail.com>
2024-10-25 14:58:03 -0700

    Update app.py

---
+++ backend/src/app.py
@@ -0,0 +1,32 @@
+class System:
+  pass
+
+class User:
+    pass
+
+class Patient:
+    pass
+
+class Worker:
+    pass
+
+class SessionManager:
+    pass
+
+class Exam:
+    pass
+
+class ReportManager:
+    pass
+
+class PermissionManager:
+    pass
+
+class Results:
+    pass
+
+class Monitor:
+    pass
+
+class EmailManager:
+    pass
