3184842b877eed616fc33071eafdc0cb29e59138
BAntalik <stich102@hotmail.com>
2024-10-30 20:50:49 -0700

    started report

--- backend/src/repositories/report_manager.py
+++ backend/src/repositories/report_manager.py
@@ -1,5 +1,6 @@
 from enum import Enum
-
+import datetime
+import db_service
 class ReportType(Enum):
     MONTHLY = "monthly"
     YEARLY = "yearly"
@@ -17,13 +18,72 @@ class ReportManager:
         Returns a list of reports associated with the given email.
         """
         # Implementation for returning reports
+
         pass
 
-    def generate_report(self):
+    def generate_report(self, year: int, month: int, userID: int):
         """
         Generates a new report.
         """
+        patientIDList = []
+        patientTestList = []
+        patientAbTestList = []
+        today = datetime.date
+
+        conn = db_service.get_db_connection
+        cursor = conn.cursor()
         # Implementation for generating a report
+        #query to get all patients
+        patientQry = "SELECT HealthID FROM patient"
+        # queries to count tests for patient within given timeframe.
+        resultQryY = "SELECT * FROM testresults WHERE healthID = ? AND YEAR (date) = ?"
+        resultQryM = "SELECT * FROM testresults WHERE healthID = ? AND MONTH(date) = ? AND YEAR (date) = ?"
+        # queries to count abnormal tests for patient within given timeframe
+        countQryY2 = """SELECT COUNT(results) FROM testresults 
+                        LEFT JOIN testtypes ON testresults.testtype = testtypes.testtype 
+                        LEFT JOIN examtable ON testresults.examid = examtable.examid
+                        WHERE examtable.healthid = ? AND YEAR (testresults.resultdate) = ?
+                        AND NOT (testtypes.lowerbound < testresults.results < testtypes.upperbound )"""
+        
+        countQryM2 = """SELECT COUNT(results)
+                        FROM testresults 
+                        LEFT JOIN testtypes ON testresults.testtype = testtypes.testtype 
+                        LEFT JOIN examtable ON testresults.examid = examtable.examid
+                        WHERE examtable.healthid =? AND YEAR (testresults.resultdate) =? AND MONTH(testresults.resultdate) =?
+                        AND NOT (testtypes.lowerbound < testresults.results < testtypes.upperbound )"""
+        MakereportQry     = """INSERT into summaryreport (workersid, monthoryear, summarydate, timeperiod)
+                            VALUES (?, ?, ?, ?);"""
+        PreMakeReportQry = "SELECT Auto_increment FROM information_schema.tables WHERE table_name='predictreports';"
+        MakeReportEntryQry  = "INSERT INTO predictreports (workersid, monthoryear, summarydate, timeperiod) VALUES(?, ?, ?, ?);"
+        cursor.execute(patientQry)
+        results = cursor.fetchall()
+
+        cursor.execute(PreMakeReportQry)
+        reportID: int = cursor.fetchone()
+        workersid: int = 1 #need method to get logged in user ID
+
+        for row in results:
+            patientIDList.append
+        # Count tests per patient
+        if month == 0:
+             for val in patientIDList:
+                 cursor.execute(resultQryY, (val, year))
+                 patientTestList.append = cursor.fetchone()
+        else:
+            for val in patientIDList:
+                 cursor.execute(resultQryM, (val, month, year))
+                 patientTestList.append = cursor.fetchone()
+        # Count unusualy tests per patient
+        if month == 0:
+             for val in patientIDList:
+                 cursor.execute(countQryY2, (val, year, month))
+                 patientAbTestList.append = cursor.fetchone()
+        else:
+            for val in patientIDList:
+                 cursor.execute(countQryM2, (val, year, month))
+                 patientAbTestList.append = cursor.fetchone()
+        cursor.execute()
+
         pass
 
     def remove_report(self, report_id: int):
