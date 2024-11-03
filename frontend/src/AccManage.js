3c54abdf8fd775bb9cac645d426550e121d7a301
IndecentTulip <jowhite2727@gmail.com>
2024-11-03 01:24:36 -0800

    added Role-Based Access Control instead of tokens

---
+++ frontend/src/AccManage.js
@@ -0,0 +1,16 @@
+import './AccManage.css';
+import { useNavigate } from 'react-router-dom';
+
+const AccManage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      create
+      delete 
+      approve
+    </div>
+  );
+};
+
+export default AccManage;
