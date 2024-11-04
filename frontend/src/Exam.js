c018f5075811df6ca80c1ec8b95742eb089b684e
Jason Yu <jasonyu6@gmail.com>
2024-11-03 21:27:19 -0800

    Made changes to exam.js

--- frontend/src/Exam.js
+++ frontend/src/Exam.js
@@ -1,6 +1,8 @@
 
 //navigation menu
 import { useNavigate } from 'react-router-dom';
+//Tracking the information filled out on the form.
+import React, { useState } from 'react';
 // if docter decides to prescribe an exam for patients
 import './DoctorMain';
 
@@ -12,23 +14,21 @@ const Exam = () =>{
     //Doctor can nvigate around the exam menu.
     const navigate = useNavigate();
     
+    // set variables from form
+    const[examId, setExamID] = useState('')
+    const[patientId, setPatientID] = useState('');
+    const[content, setContent] = useState(''); 
+
     //if user decides to prescribe exam
     if (Exam.prescribe){
-        <form>
-            var examID = readline("Enter examID");
-            var patientID = readline ("Enter patientID");
-            var content = readline ("Describe the exam"); 
-        </form>
+           //Form to fill out the info.
+           <form>
+            <input
+            type = "text"
+            value = {examID}
+            onChange={(e) => setExamID(e.target.value)}
+            placeholder="Email"
+            required/>
+           </form>
     }//if
-}
-
-class prescribe{
-     
-    //refers to patient information to itself. 
-     examID(examId, patientId, content){
-        this.examID
-        this.patientId
-        this.content
-    }//examID
-
-}//Prescribe
+}//Exam
