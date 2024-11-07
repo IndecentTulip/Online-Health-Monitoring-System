de5f16cc7090a6e854cc5de86b5453184777d53e
CrunchyCloud <hawthorneelena025@gmail.com>
2024-11-06 19:14:23 -0800

    change scripts, remove role check for now

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -9,7 +9,9 @@ const Main = () => {
   };
 
   return (
-    <div>
+    // pls don't make css for ALL the div's use classes and set csss to them
+    <div className="main">
+      
       <p>WHO ARE WE</p>
       {/* use something called "Lorem Impsum" your code editor will make a dummy text for you */}
       <p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p>
--- run_db.ps1
+++ run_db.ps1
@@ -1,2 +1,8 @@
-& ./backend/venv/Scripts/python ./backend/createdb.py
-& python ./backend/createdb.py
+& .\backend\venv\Scripts\activate.bat
+& .\backend\venv\Scripts\Activate.ps1
+
+if (-not (& ./backend/venv/Scripts/python ./backend/createdb.py)) {
+    # If the first command fails, run the second one
+    & python ./backend/createdb.py
+}
+
