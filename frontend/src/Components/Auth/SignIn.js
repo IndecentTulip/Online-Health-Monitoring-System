b9a6708ab56ebc144a3da321e1dbd98039659b9c
Sabrina <sabrinamandla17@gmail.com>
2024-11-13 15:34:21 -0800

    basic design of signin page

--- frontend/src/Components/Auth/SignIn.js
+++ frontend/src/Components/Auth/SignIn.js
@@ -13,13 +13,28 @@ const SignIn = () => {
   };
 
   return (
-    <div>
-      <button onClick={() => handleRedirectToLogin('patient')}>Patient</button>
-      <button onClick={() => handleRedirectToLogin('worker')}>Worker</button>
-      <button onClick={handleRedirectToRegister}>Don't have an account</button>
+    <div className="sign-in-page">
+      <div className="sign-in-container">
+        <div className="left-section">
+          <div className="decorative-circle"></div>
+          <div className="main-title">GET IN TOUCH</div>
+          <div className="main-subtitle">Tell us Your Problem</div>
+          <button className="register-button" onClick={handleRedirectToRegister}>
+            Register Today
+          </button>
+        </div>
+
+        <div className="right-section">
+          <button className="login-button" onClick={() => handleRedirectToLogin('patient')}>
+            Patient Login
+          </button>
+          <button className="login-button" onClick={() => handleRedirectToLogin('worker')}>
+            Staff Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default SignIn;
-
